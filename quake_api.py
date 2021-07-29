d9c7e54a6b873dd0ff401b752ce87985bb18e838
xdym <xuedongyuming2233@gmail.com>
2021-07-29 17:37:58 +0800

    搜索功能已完成，仅剩格式化输出

--- quake_api.py
+++ quake_api.py
@@ -3,22 +3,28 @@ import requests
 import json
 from prettytable import PrettyTable
 
+
 class Quake_360:
-    key = ''
+    key = '932d86eb-cb01-4a52-ae8e-c94db51cb77f'
 
     # 搜索模块
-    @staticmethod
-    def Search(key, search,size,page):
+    def Search(self, key, search, size, page):
         headers = {"X-QuakeToken": key}
 
         data = {
             "query": search,
             "start": page,
-            "size": 1
+            "size": '2'
         }
+        print(search)
         response = requests.post(url="https://quake.360.cn/api/v3/search/quake_service", headers=headers, json=data)
         r_json = json.loads(response.text)
-        print(r_json)
+        # print(r_json)
+        self.Print_search(r_json, page, size)
+
+    def Print_search(self, r_json, page, size):
+        print("\n")
+        print("页码：第{}页  共{}页  数量：{}个".format(r_json['meta']['pagination']['page_index'], r_json['meta']['pagination']['page_size'], r_json['meta']['pagination']['total']))
 
     # 版权信息
     def Copyright(self):
@@ -36,15 +42,19 @@ class Quake_360:
 
     # 命令行查询
     def Terminal(self):
-        parser = argparse.ArgumentParser(description="e.g：python quake_api.py --search IP or domain\t python quake_api.py --search \"domain=xx.com\" or \"city=Beijing\"", prog="quake_api.py")
+        parser = argparse.ArgumentParser(
+            description="e.g：python quake_api.py --search IP or domain\t python quake_api.py --search \"domain=xx.com\" or \"city=Beijing\"",
+            prog="quake_api.py")
         group = parser.add_mutually_exclusive_group()
-        group.add_argument("--version", "-V", help="Show version of 360 quake api", action='version', version="| %(prog)s v1.0 |")
+        group.add_argument("--version", "-V", help="Show version of 360 quake api", action='version',
+                           version="| %(prog)s v1.0 |")
         group.add_argument("--search", "-S", help="Search key word", type=str)
         parser.add_argument("--size", help="Show the number of results.(default=100)", default=100)
         parser.add_argument("--page", help="Show the page of results.(default=1)", default=1)
         # parser.add_argument('word_search', help="search key word")
         args = parser.parse_args()
-        self.Search(self.key, args.search, args.size, args.page)
+        if args.search:
+            self.Search(self.key, args.search, args.size, args.page)
 
 
 # main
