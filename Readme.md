9fc1ac746cd79be36d32e6502256715d08e44665
xdym <xuedongyuming2233@gmail.com>
2021-07-30 18:10:12 +0800

    Complete development

--- Readme.md
+++ Readme.md
@@ -1,7 +1,14 @@
 # 360 Quake API
+
 自用360 quake命令行查询工具
 
-未完成开发
+## 用法
+
+1. 打开quake_api.py，在第8行填入360 Quake中的API KEY
+2. 使用`pip install -r requirements.txt`安装所需要的依赖包
+3. `python quake_api.py -h`查询用法
+4. `python quake_api.py --search 搜索内容`或者`python quake_api.py -S 搜索内容`搜索资产
 
 ## 开发者
+
 [Xc1YM](https://github.com/Xc1Ym)
--- quake_api.py
+++ quake_api.py
@@ -5,7 +5,7 @@ from prettytable import PrettyTable
 
 
 class Quake_360:
-    key = '932d86eb-cb01-4a52-ae8e-c94db51cb77f'
+    key = ''  # 填360 Quake的key
 
     # 搜索模块
     def Search(self, key, search, size, page):
@@ -14,17 +14,25 @@ class Quake_360:
         data = {
             "query": search,
             "start": page,
-            "size": '2'
+            "size": '10'
         }
-        print(search)
         response = requests.post(url="https://quake.360.cn/api/v3/search/quake_service", headers=headers, json=data)
         r_json = json.loads(response.text)
         # print(r_json)
-        self.Print_search(r_json, page, size)
+        self.Print_search(r_json, page, size, search)
 
-    def Print_search(self, r_json, page, size):
+    def Print_search(self, r_json, page, size, search):
         print("\n")
-        print("页码：第{}页  共{}页  数量：{}个".format(r_json['meta']['pagination']['page_index'], r_json['meta']['pagination']['page_size'], r_json['meta']['pagination']['total']))
+        print("页码：第{}页  共{}页  总数量：{}个".format(r_json['meta']['pagination']['page_index'],
+                                              r_json['meta']['pagination']['page_size'],
+                                              r_json['meta']['pagination']['total']))
+        print("查询内容：{}".format(search))
+        len_size = len(r_json['data'])
+        table = PrettyTable(['序号', '地址', 'IP', '端口'])  # 绘制表格
+        for i in range(len_size):
+            table.add_row([i + 1, r_json['data'][i]['service']['http']['host'], r_json['data'][i]['ip'],
+                           r_json['data'][i]['port']])
+        print(table)
 
     # 版权信息
     def Copyright(self):
@@ -55,6 +63,8 @@ class Quake_360:
         args = parser.parse_args()
         if args.search:
             self.Search(self.key, args.search, args.size, args.page)
+        else:
+            print("\nusage: quake_api.py -h, --help          show help message and exit")
 
 
 # main
